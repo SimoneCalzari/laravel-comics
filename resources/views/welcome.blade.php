7eb2633d37d8476903fe9f4af86349faa7115151
SimoneCalzari <simone.calzari@outlook.it>
2024-01-26 19:52:54 +0100

    feat: aggiunte cards con dati passati tramite route

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,19 @@
 @extends('layouts.app')
+@section('main')
+    <section>
+        <div class="my-container">
+            <h2 class="text-uppercase">Current series</h2>
+            <div class="cards d-flex">
+                @foreach ($comics as $comic)
+                    <div class="my-card">
+                        <div class="img-box">
+                            <img src="{{ $comic['thumb'] }}" alt="" class="img-fluid">
+                        </div>
+                        <h4 class="text-uppercase">{{ $comic['series'] }}</h4>
+                    </div>
+                @endforeach
+            </div>
+
+        </div>
+    </section>
+@endsection
