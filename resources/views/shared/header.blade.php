f821cd8d8306f104831b28e507bbad3f5fbab267
SimoneCalzari <simone.calzari@outlook.it>
2024-01-26 18:56:00 +0100

    feat: struttura base pagina con header, hero e footer

---
+++ resources/views/shared/header.blade.php
@@ -0,0 +1,21 @@
+<header class="bg-white">
+    <div class="my-container d-flex justify-content-between">
+        <div class="logo">
+            <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="" class="img-fluid">
+        </div>
+        <nav>
+            <ul class="d-flex">
+                <li><a href="/">Characters</a></li>
+                <li><a href="/">Comics</a></li>
+                <li><a href="/">Movies</a></li>
+                <li><a href="/">Tv</a></li>
+                <li><a href="/">Games</a></li>
+                <li><a href="/">Collectibles</a></li>
+                <li><a href="/">Videos</a></li>
+                <li><a href="/">Fans</a></li>
+                <li><a href="/">News</a></li>
+                <li><a href="/">Shop</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
