e72beace30b844fa1cf3ffdda9732547592d2e39
SimoneCalzari <simone.calzari@outlook.it>
2024-01-26 21:09:55 +0100

    feat: esercizio completato

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 @section('main')
-    <section>
+    <section id="main-content">
         <div class="my-container">
             <h2 class="text-uppercase">Current series</h2>
             <div class="cards d-flex">
@@ -13,7 +13,46 @@
                     </div>
                 @endforeach
             </div>
-
+            <div class="row-btn">
+                <button class="text-uppercase">Load more</button>
+            </div>
+        </div>
+    </section>
+    <section id="icons-list">
+        <div class="my-container">
+            <ul class="d-flex ">
+                <li class="d-flex">
+                    <div class="img-box">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png') }}"
+                            :alt="merch.text" />
+                    </div>
+                    <a href="/" class="text-uppercase">Digital comics</a>
+                </li>
+                <li class="d-flex">
+                    <div class="img-box">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png') }}" :alt="" />
+                    </div>
+                    <a href="/" class="text-uppercase">Dc merchandise</a>
+                </li>
+                <li class="d-flex">
+                    <div class="img-box">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png') }}" :alt="" />
+                    </div>
+                    <a href="/" class="text-uppercase">Subscription</a>
+                </li>
+                <li class="d-flex">
+                    <div class="img-box">
+                        <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png') }}" :alt="" />
+                    </div>
+                    <a href="/" class="text-uppercase">Comic shop locator</a>
+                </li>
+                <li class="d-flex">
+                    <div class="img-box">
+                        <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg') }}" :alt="" />
+                    </div>
+                    <a href="/" class="text-uppercase">Dc power visa</a>
+                </li>
+            </ul>
         </div>
     </section>
 @endsection
